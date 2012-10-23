fd2aae461581bad79b5f2bc0336ccd48a9f8e061
Marco <marco@marco-ThinkPad-T61.(none)>
2012-10-23 20:15:19 +0200

    Tests use EUnit

--- erlang/mr/mr_test.erl
+++ erlang/mr/mr_test.erl
@@ -1,11 +1,17 @@
+-include_lib("eunit/include/eunit.hrl").
 -module(mr_test).
 -compile(export_all).
 
-test_init_stop() ->
+init_stop() ->
     {ok, CPid} = mr:start(3),
-    mr:stop(CPid).
+    mr:stop(CPid),
+    CPid.
 
-test_sum() ->
+init_stop_test() ->
+    Pid=init_stop(),
+    ?assert(is_pid(Pid) and not is_process_alive(Pid)).
+
+sum() ->
     {ok,MR} = mr:start(3),
     {ok,Sum} = mr:job(MR,
 		      fun(X) -> X end,
@@ -15,17 +21,22 @@ test_sum() ->
     mr:stop(MR),
     Sum.
 
-test_fac() ->
+sum_test() -> ?assert(sum() =:= 55).
+
+fac(N) ->
     {ok,MR} = mr:start(3),
     {ok,Fac} = mr:job(MR,
 		      fun(X) -> X end,
 		      fun(X,Acc) -> X*Acc end,
 		      1,
-		      lists:seq(1,10)),
+		      lists:seq(1,N)),
     mr:stop(MR),
     Fac.
 
-test_fac_sum() ->
+fac_test() ->
+    ?assert(fac(6) =:= 720).
+
+fac_sum() ->
     {ok,MR} = mr:start(3),
     {ok,Sum} = mr:job(MR,
 		      fun(X) -> X end,
@@ -40,7 +51,11 @@ test_fac_sum() ->
     mr:stop(MR),
     {Sum,Fac}.
 
-test_word_count() ->
+fac_sum_test() ->
+    {Sum, Fac} = fac_sum(),
+    ?assert((Sum =:= 55) and (Fac =:= 3628800)).
+
+word_count() ->
     {ok,MR} = mr:start(3),
     {ok,Dict} = mr:job(MR,
 		       fun(X) -> {X, 1} end,
@@ -51,3 +66,10 @@ test_word_count() ->
 		       ["Hello", "World", "Goodbye", "World"]),
     mr:stop(MR),
     dict:to_list(Dict).
+
+word_count_test() ->
+    Res=word_count(),
+    ?assert(Res=:=[{"Hello", 1}
+                  ,{"Goodbye", 1}
+                  ,{"World", 2}]).
+    
